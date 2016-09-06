f4b38fa7c9e3d83cd01245f2cfa2b2883d25f8c4
PeterBoss <peter2137@gmail.com>
2016-09-06 15:24:09 +0200

    thread safe, observer

--- src/main/java/cagrp/ca1/ClientHandler.java
+++ src/main/java/cagrp/ca1/ClientHandler.java
@@ -3,11 +3,13 @@ package cagrp.ca1;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ClientHandler implements Runnable {
+public class ClientHandler extends Thread implements Observer {
 
     private final Socket socket;
     private final PrintWriter writer;
@@ -33,19 +35,22 @@ public class ClientHandler implements Runnable {
                 } else if (message.equals("LOGOUT:")) {
                     break;
                 }
-                //temp (very temp)
-                writer.println("test answer");
+                server.sendMessage(message);
             }
         } finally {
             try {
-                writer.println("STOP");//Echo the stop message back to the client for a nice closedown
+                writer.println("LOGGING OUT");//Echo the stop message back to the client for a nice closedown
                 socket.close();
-                // remove observer
-                //server.removeHandler(this);
+                server.deleteObserver(this);
                 System.out.println("Closed a Connection");
             } catch (IOException ex) {
                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     } 
+
+    @Override
+    public void update(Observable o, Object arg) {
+        writer.println(arg);
+    }
 }
