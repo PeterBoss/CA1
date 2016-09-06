ec46ba5724888c4a22fb4d7c0ce8a882c574d098
Joseph Awwal <josephawwal@Josephs-MacBook-Pro.local>
2016-09-06 13:59:59 +0200

    test apple commit

---
+++ src/main/java/cagrp/EchoClient/EchoListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EchoClient;
+
+/**
+ *
+ * @author josephawwal
+ */
+public interface EchoListener {
+    
+    
+    void messageArrived(String message);
+    
+}
